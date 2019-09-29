9650387c77cd850f3559fa073d7783d3910485f9
Nick Jones <nrjones8@gmail.com>
2019-09-29 10:51:28 -0400

    add /explorer page with heatmap. v unpolished for now

--- gatsby-ballot-props-site/gatsby-node.js
+++ gatsby-ballot-props-site/gatsby-node.js
@@ -1,17 +1,69 @@
 // Thank you https://dev.to/ashleemboyer/build-a-multi-page-gatsby-site-from-json-3kp
 const path = require('path');
 const ballotPropsData = require('../data/ballot_measure_history.json');
+const _ = require('lodash');
+
+
+function prepDataForApexHeatmap(allPropsData) {
+    // Format of data for an apex heatmap is a lil confusing, see:
+    // https://apexcharts.com/docs/chart-types/heatmap-charts/
+    const groupedByDate = _.groupBy(allPropsData, 'election_date');
+    let forApex = [];
+
+    // TODO clean this up, there are definitely better ways to do this...
+    for (date in groupedByDate) {
+        let allProps = groupedByDate[date];
+        let propsForThisDate = []
+
+        for (index in allProps) {
+          propsForThisDate.push({
+            // Required for the heatmap
+            x: allProps[index].prop_letter_or_num,
+            y: allProps[index].pct_yes_votes - allProps[index].pct_required_to_pass,
+          
+            // Extra context, e.g. to display in a tooltip
+            numYesVotes: allProps[index].num_yes_votes,
+            numNoVotes: allProps[index].num_no_votes,
+            description: allProps[index].description,
+            title: allProps[index].prop_title, 
+          });
+        }
+        forApex.push({
+            // These two are necessary for apex's heatmap
+            name: date,
+            data: propsForThisDate,
+
+            // Everything else is for our own use
+            // e.g. from "11/5/1907"
+            electionYear: parseInt(date.split('/')[2]),
+        });
+    }
+
+    return forApex;
+}
 
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 
-  const template = path.resolve('./src/templates/ballot-prop.js');
+  const singlePropTemplate = path.resolve('./src/templates/ballot-prop.js');
+  const explorerTemplate = path.resolve('./src/templates/heatmap-explorer.js');
 
   ballotPropsData.forEach(propObject => {
     createPage({
         path: `${propObject.election_year}/${propObject.election_month}/${propObject.election_day}/${propObject.prop_letter_or_num}`,
-        component: template,
+        component: singlePropTemplate,
         context: propObject,
     });
   });
+
+  // Prepare data for the heatmap/explorer
+  const heatmapData = prepDataForApexHeatmap(ballotPropsData);
+
+  createPage({
+    path: 'explorer',
+    component: explorerTemplate,
+    context: heatmapData,
+  })
+
+
 }
---
+++ gatsby-ballot-props-site/src/templates/heatmap-explorer.js
@@ -0,0 +1,75 @@
+import React from "react"
+import Chart from "react-apexcharts";
+
+import Layout from "../components/layout"
+
+const DEFAULT_MIN_YEAR = 1980;
+
+export default class BallotPropTemplate extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          propsData: Object.values(props.pageContext).filter(p => p.electionYear >= DEFAULT_MIN_YEAR),
+          options: this.getHeatmapOptions()
+        };
+    }
+
+    getHeatmapOptions() {
+      return {
+        plotOptions: {
+          heatmap: {
+            shadeIntensity: 0.5,
+            reverseNegativeShade: true,
+
+            colorScale: {
+              ranges: [
+                {
+                  from: -40,
+                  to: 0,
+                  name: 'did not passs',
+                  color: '#FF0000'
+                },
+                {
+                  from: 0,
+                  to: 40,
+                  name: 'passed',
+                  color: '#128FD9'
+                }
+              ]
+            }
+          },
+        },
+        // TODO this looks terrible right now
+        xaxis: {
+          position: 'top',
+          offsetY: -40,
+        },
+        dataLabels: {
+          enabled: false
+        },
+        title: {
+          text: 'HeatMap Chart with Color Range'
+        },
+      }
+    }
+
+    render() {
+      console.log(this.props.pageContext);
+        return (
+            <Layout>
+                <div className="row">
+                  <div className="mixed-chart">
+                    <Chart
+                      type="heatmap"
+                      options={this.state.options}
+                      series={this.state.propsData}
+                      // width="800"
+                      height="1000"
+                    />
+                  </div>
+                </div>
+            </Layout>
+        )
+  }
+}
